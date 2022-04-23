105e164efd10b4703d25641612682bcfffcb3b2f
Seeun Choi <conia@naver.com>
2022-04-23 12:51:38 +0100

    fixed typo in the file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -7,3 +7,16 @@ public class Hello {
         System.out.println("Local repo is published ro github");
     }
 }
+
+/* fetch 방법
+github.com 에서 add file 클릭
+create new file 클릭
+name your file 이름 짓기
+코드 작성후 아래로 내려가서  commit new file 에 코밋 달기
+더 아래에 commit new file 버튼 클릭
+
+인텔리제이로 와서 Git -> Fetch 클릭
+remote origin master 를 local 의 master 로 데려오기 위해
+Git -> pull -> pull 클릭
+
+ */
